dbe519b085abd1a671437a3b741efa949835ab0e
tudorhutu <tudorhutu2012@gmail.com>
2021-10-15 12:05:16 +0300

    all of it
    it's all of it

---
+++ My project (4)/Assets/basketball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basketball : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        gameObject.SetActive(false);
+    }
+}
